9f84260c4587ab567b0605171582d3e3dc8a6dca
Sam <samo.hakobyan.22@gmail.com>
2021-05-25 12:52:20 +0400

    first commit

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -6,64 +6,69 @@ import { Day } from '../models/day.model';
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
-export class CalendarComponent{
+export class CalendarComponent {
 
   weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   emptyStartDivs = [];
   emptyEndDivs = [];
-  importantDay:string;
-  
+  importantDay: string;
+
   thisDay = new Date();
 
   month = this.thisDay.getMonth();
   year = this.thisDay.getFullYear();
 
-  oneMontsDate:[Date];
+  oneMontsDate: [Date];
 
-  constructor(){
-    this.createMonth(this.year,this.month);
+  constructor() {
+    this.createMonth(this.year, this.month);
   }
 
-  createMonth(year:number, month:number){
+  createMonth(year: number, month: number) {
     const dayOneForMonth = new Date(year, month, 1);
     this.oneMontsDate = [null];
     this.emptyStartDivs = [];
     this.emptyEndDivs = [];
 
-    for (let i = 0; dayOneForMonth.getMonth()===month; i++) {
+    for (let i = 0; dayOneForMonth.getMonth() === month; i++) {
       this.oneMontsDate[i] = new Date(dayOneForMonth.toDateString());
-      dayOneForMonth.setDate(dayOneForMonth.getDate()+1);
+      dayOneForMonth.setDate(dayOneForMonth.getDate() + 1);
     }
 
     for (let i = 0; i < this.oneMontsDate[0].getDay(); i++) {
       this.emptyStartDivs[i] = i;
     }
 
-    for (let i = 0; i < 6 - this.oneMontsDate[this.oneMontsDate.length-1].getDay(); i++) {
+    for (let i = 0; i < 6 - this.oneMontsDate[this.oneMontsDate.length - 1].getDay(); i++) {
       this.emptyEndDivs[i] = i;
     }
   }
 
-  prev(){
+  prev() {
     this.month--;
-    if(this.month<0){
-      this.month=11;
+    if (this.month < 0) {
+      this.month = 11;
       this.year--;
     }
-    this.createMonth(this.year,this.month);
+    this.createMonth(this.year, this.month);
   }
 
-  next(){
+  next() {
     this.month++;
-    if(this.month>11){
-      this.month=0;
+    if (this.month > 11) {
+      this.month = 0;
       this.year++;
     }
-    this.createMonth(this.year,this.month);
+    this.createMonth(this.year, this.month);
   }
 
-  addImportantDay(day:Date){
-    this.importantDay = day.toDateString();
+  addImportantDay(day: Date) {
+    if (this.importantDay === day.toDateString()) {
+      this.importantDay = '';
+    }
+    else {
+      this.importantDay = day.toDateString();
+    }
   }
 
 }
